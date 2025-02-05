de0fe0939861a48e035840b6d2906d17c7085f30
Ismail Tarik <120662023+ismailtarik@users.noreply.github.com>
2025-02-05 11:29:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ var cookieParser = require('cookie-parser');
 var morgan = require('morgan');  // morgan logger
 var Web3 = require('web3');
 
-// Establish connection with Alchemy Sepolia
-const web3 = new Web3('https://eth-sepolia.g.alchemy.com/v2/QOp6tWYTJ1a_1M_bAuKOIR989euSGZ2Z');
+// // Establish connection with Alchemy Sepolia
+// const web3 = new Web3('https://eth-sepolia.g.alchemy.com/v2/QOp6tWYTJ1a_1M_bAuKOIR989euSGZ2Z');
 
-// Get contract address
-const contractAddress = '0x09bd209B22D39cbfeF9C4ce0543Cf40cF707B1ee';
+// // Get contract address
+// const contractAddress = '0x09bd209B22D39cbfeF9C4ce0543Cf40cF707B1ee';
 
 // I M P O R T     C O M P I L E D     O U T P U T     O F     M E D I C A L C H A I N     C O N T R A C T     F R O M     B U I L D     D I R E C T O R Y
 var MyContractJSON = require(path.join(__dirname,'build/contracts/MedicalChain.json'));
 
-// // E S T A B L I S H     C O N N E C T I O N     W I T H     L O C A L     G E T H     P R I V A T E     C H A I N
-// web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+// E S T A B L I S H     C O N N E C T I O N     W I T H     L O C A L     G E T H     P R I V A T E     C H A I N
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
-// // G E T     C O N T R A C T     A D D R E S S ,   F R O M     N E T W O R K     I D     4 0 0 2  (N/W ID OF PRIVATE CHAIN)
-// contractAddress = MyContractJSON.networks['5777'].address;
+// G E T     C O N T R A C T     A D D R E S S ,   F R O M     N E T W O R K     I D     4 0 0 2  (N/W ID OF PRIVATE CHAIN)
+contractAddress = MyContractJSON.networks['5777'].address;
 
 // C O N T R A C T     A D D R E S S
 console.log("\nC O N T R A C T     A D D R E S S : "+contractAddress+"\n");
